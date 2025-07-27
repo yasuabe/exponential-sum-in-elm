859799bae9dd7f187376f12282f44727b5627c91
yasuabe2613 <yasu@yasuabe.net>
2025-07-27 15:52:49 +0900

    implement requirements

---
+++ tests/ExponentialSumTest.elm
@@ -0,0 +1,21 @@
+module ExponentialSumTest exposing (..)
+
+import Expect
+import Main
+import Test exposing (..)
+
+
+suit : Test
+suit =
+    describe "ExponentialSum"
+        [ test "degration stopper for refactoring" <|
+            \() ->
+                Main.encodeExpSums 25 3 26
+                    |> String.left 100
+                    |> (Expect.equal <| String.left 100 expected2)
+        ]
+
+
+expected2 : String
+expected2 =
+    "264.06647466976415,78.58323241581222 277.5490823588306,86.92779669774518 261.69309096275003,86.92779"
